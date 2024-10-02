628b883aeb5ba8b6274a8788b0029ea74d5e1193
Amit Kumar <144479526+Amit0022R@users.noreply.github.com>
2024-10-02 23:59:17 +0530

    Promises

---
+++ Javascript/Promises1.js
@@ -0,0 +1,60 @@
+// class Rectangle {
+//     constructor(width, height, color) {
+//          this.width = width;
+//          this.height = height;
+//          this.color = color; 
+//     }
+
+// const { log } = require("async");
+
+    
+//     area() {
+//         const area = this.width * this.height;
+//           return area;
+//     }
+    
+//     paint() {
+//              console.log(`Painting with color ${this.color}`);
+//     }
+    
+//  }
+ 
+//  const rect = new Rectangle(2, 4, "red") // instance of rectangle class , object of rectangle class
+//  const area = rect.area();
+//  console.log(area)
+//  rect.paint();
+
+
+// const currentdate = new Date();
+// console.log(currentdate); // 2024-10-02T12:13:39.908Z
+//console.log(currentdate.getMonth()); // 9 (becz jan=>0, feb=>1)
+// console.log(currentdate.getFullYear()); // 2024
+
+
+
+// const map = new Map();
+// map.set('name', 'Alice');
+// map.set('age', 30);
+// console.log(map.get('name'));
+
+
+//  ******************** PROMISES ************************//
+
+
+// return an object of promise class
+// function setTimeoutPromisified(ms) {
+//     return new Promise(resolve => setTimeout(resolve, ms));
+//   }
+//   function callback() {
+//       console.log("3 seconds have passed");
+//   }
+//   setTimeoutPromisified(3000).then(callback)
+  
+
+// function main() {
+
+// }
+// let p = new Promise(main);
+// console.log(p);\
+
+
