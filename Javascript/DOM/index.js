bede61262eb1806a705cc6a8bf9ea3f4ed21149e
Amit Kumar <144479526+Amit0022R@users.noreply.github.com>
2024-10-06 16:26:41 +0530

    delete

---
+++ Javascript/DOM/index.js
@@ -0,0 +1,15 @@
+// let ctr = 0;
+// function callback() {
+//     const el = document.querySelectorAll("h4")[1]
+//     el.innerHTML = ctr;
+//     ctr = ctr + 1;
+// }
+// setInterval(callback,1000);
+
+
+// function addTodo() {
+//     const inputElement = document.querySelector("input")
+//     const value = inputElement.value;
+//     console.log(value);
+//     }
+
